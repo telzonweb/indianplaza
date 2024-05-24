4671f0ee4c4bcec5d30c450697e12d065fe3e514
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-05-24 13:27:14 +0530

    "Updated CuminStory component: changed SectionTitle props, title text, and removed an empty line"

--- src/components/sections/CuminStory.tsx
+++ src/components/sections/CuminStory.tsx
@@ -6,13 +6,12 @@ const CuminStory = () => {
     <div className="lg:p-10">
       <SectionTitle
         subtitle="Truly Indian"
-        sectionClasses="mx-auto max-w-xl text-center m-12"
-        titleClasses="mb-3 text-center"
-        subtitleClasses="text-md font-large"
+        sectionClasses="mx-auto max-w-xl text-center mb-12"
+        titleClasses="mb-3 text-center "
+        subtitleClasses="text-md font-large "
       >
-        More than Grocery
+        Our Roots
       </SectionTitle>
-
       <div className="relative mx-[5%] flex flex-col gap-1 rounded-xl shadow-sm md:flex-row lg:mx-[10%] lg:p-6">
         <div className="relative z-10 h-auto  rounded-lg bg-[#ffd9a191] p-5 md:w-1/2 lg:p-10 lg:px-16 lg:py-20 ">
           <h1 className="mb-4 font-Caveat text-4xl font-bold">
