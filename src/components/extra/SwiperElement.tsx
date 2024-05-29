a442e406f5967da79f7e04d9fd8006a7334b9448
Vipul Dhomane <106507333+vipuldhomane@users.noreply.github.com>
2024-05-29 18:04:13 +0530

    "Removed carousel1.jpg image and updated imgArray in SwiperElement.tsx with new image paths."

--- src/components/extra/SwiperElement.tsx
+++ src/components/extra/SwiperElement.tsx
@@ -10,9 +10,10 @@ import "swiper/css/pagination";
 import { Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const imgArray = [
-  "/mainImages/carousel1.jpg",
-  "https://img.freepik.com/free-photo/top-view-wooden-bowls-with-rice-corn-seeds-pumpkin-seeds-beans-lentiles-thin-pasta-different-spices-grey-background_140725-145832.jpg?t=st=1716446566~exp=1716450166~hmac=1a95b0fcfb15fd57ca805c6a87dbecd8ca3f22fbb5ff7c17a1fb965ec1007335&w=740",
-  "https://img.freepik.com/free-photo/flat-lay-tasty-jewish-food-assortment_23-2148814454.jpg?t=st=1716446670~exp=1716450270~hmac=2671e8047785ce1ddbce28fcc64fffca92b41d4a19978e47765cc5ce8f6485a1&w=996",
+  "/mainImages/carausel/img1.jpg",
+  "/mainImages/carausel/img4.jpg",
+  "/mainImages/carausel/img2.jpg",
+  "/mainImages/carausel/img3.jpg",
 ];
 
 export const SwiperElement = () => {
